8cb5dafbf164ca509bbefcd0d90ff49bfeb582af
srajput0 <pshycho566@gmail.com>
2025-07-22 19:23:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = "7882173382:AAGFqT3FNolrkIHmn83uG2CsFDS1bDgBZ-s"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("🎲 Play 3D Ludo", callback_game="ludo3d")]]
+    keyboard = [
+        [InlineKeyboardButton("🎮 Play Ludo", url="https://ludonsnbs.duckdns.org")],
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
-        "Click below to play 3D Ludo!", reply_markup=reply_markup
+        "Welcome to Ludo Game! 🎲\nClick below to start playing 👇",
+        reply_markup=reply_markup
     )
 
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+if __name__ == "__main__":
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
